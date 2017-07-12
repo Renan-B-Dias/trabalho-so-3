999fb80f3749c5a8977d0a7a228a0ab23479e513
JonathanHeidy <vashjhk@gmail.com>
2017-07-12 10:31:45 -0300

    Adição do código de tratamento de exceções e da fila de pendência no método de put

--- src/MainMemoryFirstFit.java
+++ src/MainMemoryFirstFit.java
@@ -5,88 +5,112 @@ import java.util.*;
  */
 public class MainMemoryFirstFit {
 
-    private Segment[] mainMemory;
-    private int nSegments;
+	private Segment[] mainMemory;
+	private int nSegments;
+	
+	
+	private PriorityQueue<Segment> pendingQueue;
 
-    private PriorityQueue<Segment> pendingQueue;
+	public MainMemoryFirstFit(int mainBytes) {
+		mainMemory = new Segment[mainBytes];
+		this.nSegments = mainBytes;
+		this.pendingQueue = new PriorityQueue<>();
+	}
 
-    public MainMemoryFirstFit(int mainBytes) {
-        mainMemory = new Segment[mainBytes];
-        this.nSegments = mainBytes;
-        this.pendingQueue = new PriorityQueue<>();
-    }
+	public void put(Segment seg) {
+		int count = 0;
+		boolean hasSpace = false;
 
-    public void put(Segment seg) {
-        int count = 0;
-        boolean hasSpace = false;
+		try {
+			if(seg.segId < 0 || seg.segId > nSegments - 1){
+				throw new Exception("Erro1");
+			}else{
+				for (int j = 0; j < mainMemory.length; j++) {
+					if (mainMemory[j] != null )
+						if(mainMemory[j].segId == seg.segId)
+							throw new Exception("Erro2");
+				}
+				
+				
+			}
+			
+			
+			int i = 0;
+			for (; i < mainMemory.length && !hasSpace; i++) {
+				if (mainMemory[i] != null)
+					count = 0;
+				else
+					count++;
 
-        int i = 0;
-        for(; i < mainMemory.length && !hasSpace; i++) {
-            if(mainMemory[i] != null)
-                count = 0;
-            else
-                count++;
+				if (count == seg.segSize) {
+					hasSpace = true;
+					i--;
+				}
+			}
 
+			if (hasSpace){
+				for (int size = seg.segSize; size > 0; size--, i--)
+					mainMemory[i] = seg;
+				System.out.println("Segmento "+ seg.segId +" foi inserido na memória");
+			}
+			else {
+				pendingQueue.add(seg);
+				System.out.println("No space");
+			}
 
-            if(count == seg.segSize) {
-                hasSpace = true;
-                i--;
-            }
-        }
+		} catch (Exception e) {
+			if(e.getMessage().equals("Erro1")){
+				System.out.println("Segmento "+ seg.segId + " inválido!");
+			}else if(e.getMessage().equals("Erro2")){
+				System.out.println("Segmento "+ seg.segId + " já está na memória!");
+			}
+		}
+	}
 
-        if(hasSpace)
-            for(int size = seg.segSize; size > 0; size--, i--)
-                mainMemory[i] = seg;
+	public void printAll() {
+		for (Segment x : mainMemory)
+			if (x == null)
+				System.out.printf("%s ", "null");
+			else
+				System.out.printf("%d ", x.segId);
+	}
 
-        else {
-            System.out.println("No space");
-        }
-    }
-
-    public void printAll() {
-        for(Segment x: mainMemory)
-            if(x == null)
-                System.out.printf("%s ", "null");
-            else
-                System.out.printf("%d ", x.segId);
-    }
-
-//    public MainMemory(int mainBytes) {
-//        this.mainMemory = new Segment[mainBytes];
-//        this.nSegments = mainBytes;
-//        this.pendingQueue = new PriorityQueue<>();
-//    }
-//
-//    public void putSeg(Segment seg) {
-//        if(seg.segId < 0 || seg.segId > nSegments-1) {
-//            System.out.println("â€œSegmento " + seg.segId + " invalido!");
-//        } else if(inMemory(seg)) {
-//            System.out.println("â€œSegmento " + seg.segId + " ja esta na memoria!");
-//        } else if(isFull()) { // see memory size
-//            pendingQueue.add(seg);  /*pendingQueue.remove();*/
-//        } else {
-//
-//        }
-//    }
-//
-//    public void delSeg(Segment seg) {
-//        if(seg.segId < 0 || seg.segId >nSegments-1)
-//            System.out.print("Numero de segmento " + seg.segId + " invalido!");
-//        else if()
-//            System.out.print("Segmento seg nao esta na memoria!");
-//        else {
-//            pendingQueue.remove(seg);
-//
-//
-//        }
-//    }
-//
-//    private Boolean inMemory(Segment seg) {
-//        return false;
-//    }
-//
-//    private Boolean isFull() {
-//        return false;
-//    }
+	// public MainMemory(int mainBytes) {
+	// this.mainMemory = new Segment[mainBytes];
+	// this.nSegments = mainBytes;
+	// this.pendingQueue = new PriorityQueue<>();
+	// }
+	//
+	// public void putSeg(Segment seg) {
+	// if(seg.segId < 0 || seg.segId > nSegments-1) {
+	// System.out.println("â€œSegmento " + seg.segId + " invalido!");
+	// } else if(inMemory(seg)) {
+	// System.out.println("â€œSegmento " + seg.segId + " ja esta na memoria!");
+	// } else if(isFull()) { // see memory size
+	// pendingQueue.add(seg); /*pendingQueue.remove();*/
+	// } else {
+	//
+	// }
+	// }
+	//
+	// public void delSeg(Segment seg) {
+	// if(seg.segId < 0 || seg.segId >nSegments-1)
+	// System.out.print("Numero de segmento " + seg.segId + " invalido!");
+	// else if()
+	// System.out.print("Segmento seg nao esta na memoria!");
+	// else {
+	// pendingQueue.remove(seg);
+	//
+	//
+	// }
+	// }
+	//
+	// private Boolean inMemory(Segment seg) {
+	// return false;
+	// }
+	//
+	// private Boolean isFull() {
+	// return false;
+	// }
 
 }
