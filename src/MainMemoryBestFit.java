64ce3c94d188fdb75646e2fa802affebc586e3fc
Renan Dias <renan@jera.com.br>
2017-07-12 13:36:14 -0400

    Best fit not working yet

--- src/MainMemoryBestFit.java
+++ src/MainMemoryBestFit.java
@@ -16,7 +16,11 @@ public class MainMemoryBestFit {
         this.pendingQueue = new PriorityQueue<>();
     }
 
-    private Boolean put(Segment seg) {
+    public void put(Segment seg) {
+        put(seg, false);
+    }
+
+    private Boolean put(Segment seg, boolean msg) {
         if(seg.segId < 0 || seg.segId > nSegments-1) {
             System.out.println("Segmento " + seg.segId + " invalido!");
             return false;
@@ -27,70 +31,46 @@ public class MainMemoryBestFit {
             return false;
         }
         else {
-//            int count = 0;
-//            int begin = 0, end = 0;
-//            boolean hasSpace = false;
-//
-//
-//
-//            int i = 0;
-//            for(; i < mainMemory.length; i++) {
-//
-//            }
-            return true;
+
+            int count = 0;
+            ArrayList<SegmentControll> array = new ArrayList<>();
+
+            int i = 0;
+            for(; i < mainMemory.length; i++) {
+                count = 0;
+
+                while(mainMemory[i++] == null) {
+                    count++;
+                    if(i == 13)
+                        break;
+                }
+
+                if(count > seg.segSize)
+                    array.add(new SegmentControll((i - count), i-1));
+
+            }
+
+            if(array.isEmpty()) {
+                pendingQueue.add(seg);
+                return false;
+            }
+
+            else {
+
+                Collections.sort(array);
+
+                SegmentControll least = array.get(0);
+
+                int begin = least.begin;
+                int siz = seg.segSize;
+
+                for(int f = 0; f < siz ; f++, begin++) {
+                    mainMemory[begin] = seg;
+                }
+                return true;
+            }
         }
     }
-//        else {
-//            int count = 0;
-//            boolean hasSpace = false;
-//
-//            int i = 0;
-//            for (; i < mainMemory.length && !hasSpace; i++) {
-//                if (mainMemory[i] != null)
-//                    count = 0;
-//                else
-//                    count++;
-//
-//
-//                if (count == seg.segSize) {
-//                    hasSpace = true;
-//                    i--;
-//                }
-//            }
-//
-//            if (hasSpace) {
-//                for (int size = seg.segSize; size > 0; size--, i--)
-//                    mainMemory[i] = seg;
-//                if(msg)
-//                    System.out.println("Segmento " + seg.segId + " foi inserido na memoria!");
-//                return true;
-//            }
-//            else {
-//                pendingQueue.add(seg);  /*pendingQueue.remove();*/
-//                return false;
-//            }
-//        }
-//    }
-//
-//    public void delSeg(Segment seg) {
-//        if(seg.segId < 0 || seg.segId > nSegments)
-//            System.out.println("Numero de segmento " + seg.segId + " invalido");
-//        else if(!inMemory(seg.segId))
-//            System.out.println("Segmento " + seg.segId + " não esta na memoria");
-//        else
-//            remove(seg.segId);
-//
-//        if(pendingQueue.size() != 0) {
-//            Segment pended[] = new Segment[pendingQueue.size()];
-//            pended = pendingQueue.toArray(pended);
-//            pendingQueue = new PriorityQueue<>();
-//            for(Segment x: pended) {
-//                Boolean inserted = put(x, false);
-//                if(inserted)
-//                    System.out.println("Segmento " + x.segId + " que estava na lista de pendencias foi inserido na memoria");
-//            }
-//        }
-//    }
 
     public void printAllMain() {
         for(Segment x: mainMemory)
@@ -105,7 +85,7 @@ public class MainMemoryBestFit {
             System.out.printf("%d ", x.segId);
     }
 
-    private void remove(int segId) {
+    public void remove(int segId) {
         for(int i = 0; i < mainMemory.length; i++)
             if(mainMemory[i] != null)
                 if(mainMemory[i].segId == segId)
---
+++ src/SegmentControll.java
@@ -0,0 +1,26 @@
+/**
+ * Created by yellow-umbrella on 12/07/17.
+ */
+public class SegmentControll implements Comparable<SegmentControll> {
+
+    protected int begin;
+    protected int end;
+
+    protected int size;
+
+    public SegmentControll(int begin, int end) {
+        this.begin = begin;
+        this.end = end;
+        size = end - begin;
+    }
+
+    @Override
+    public int compareTo(SegmentControll seg) {
+        if(this.size > seg.size)
+            return 1;
+        else if(this.size < seg.size)
+            return -1;
+        return 0;
+    }
+
+}
